535369235e6ec08a3e1f7a9cc39815217908bb20
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-20 10:15:31 +0545

    Added to the installed app

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'core', 
+    'dashboard',
+    'payments',
+    'properties',
+    
+    
 ]
 
 MIDDLEWARE = [
