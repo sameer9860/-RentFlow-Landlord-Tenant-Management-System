47f33f9aa9a20fadc5eb94c328ae0c216d8a3321
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-22 08:37:42 +0545

    Added model to admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import RentInvoice, Payment
 
-# Register your models here.
+class PaymentInline(admin.TabularInline):
+    model = Payment
+    extra = 1
+
+@admin.register(RentInvoice)
+class RentInvoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tenancy', 'month', 'year', 'amount', 'due_date', 'status', 'generated_at')
+    list_filter = ('status', 'month', 'year', 'due_date')
+    search_fields = ('tenancy__tenant__username', 'tenancy__property__name', 'tenancy__room__room_number')
+    inlines = [PaymentInline]
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'invoice', 'paid_amount', 'payment_date', 'method', 'transaction_id')
+    list_filter = ('method', 'payment_date')
+    search_fields = ('invoice__id', 'transaction_id')
+
