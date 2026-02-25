df2b8c4c2d2d3dcc51715b4d8bc12994756e168a
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:48:29 +0545

    First dash url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from .views import DashboardRedirectView
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', DashboardRedirectView.as_view(), name='dashboard_redirect_root'),
     path('dashboard-redirect/', DashboardRedirectView.as_view(), name='dashboard_redirect'),
 ]
