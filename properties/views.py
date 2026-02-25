281148cf9f2c1c4d628724993a89ae343c02a3e5
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:27:51 +0545

    Updated room and property

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Property, Room
 
-# Create your views here.
+class PropertyListView(LoginRequiredMixin, ListView):
+    model = Property
+    template_name = 'properties/property_list.html'
+    context_object_name = 'properties'
+
+    def get_queryset(self):
+        return Property.objects.filter(landlord=self.request.user)
+
+class RoomListView(LoginRequiredMixin, ListView):
+    model = Room
+    template_name = 'properties/room_list.html'
+    context_object_name = 'rooms'
+
+    def get_queryset(self):
+        return Room.objects.filter(property__landlord=self.request.user)
