19454595570ea7ce9dd2e8534f808dcbee914049
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:36:12 +0545

    Created dashboard redirect view

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import DashboardRedirectView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('dashboard-redirect/', DashboardRedirectView.as_view(), name='dashboard_redirect'),
+]
