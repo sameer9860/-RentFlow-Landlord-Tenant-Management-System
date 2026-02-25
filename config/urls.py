c159c404603f5b4d3d85bc44623e28032a895f37
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:37:17 +0545

    Added account url

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('properties/', include('properties.urls')),
     path('payments/', include('payments.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
