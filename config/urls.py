2f1ddf7027a7025e77a9ca4e4606c394a18870e1
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:49:03 +0545

    Account url on empty

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('accounts.urls')), 
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
     path('properties/', include('properties.urls')),
