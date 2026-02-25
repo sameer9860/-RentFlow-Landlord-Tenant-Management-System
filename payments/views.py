1a2c530bef43f27a7b447134a0e4d2b0df335133
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:26:29 +0545

    Updated invoice list

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import RentInvoice
 
-# Create your views here.
+class InvoiceListView(LoginRequiredMixin, ListView):
+    model = RentInvoice
+    template_name = 'payments/invoice_list.html'
+    context_object_name = 'invoices'
+
+    def get_queryset(self):
+        user = self.request.user
+        if hasattr(user, 'profile') and user.profile.role == 'LANDLORD':
+            return RentInvoice.objects.filter(tenancy__room__property__landlord=user).order_by('-year', '-month')
+        else:
+            return RentInvoice.objects.filter(tenancy__tenant=user).order_by('-year', '-month')
