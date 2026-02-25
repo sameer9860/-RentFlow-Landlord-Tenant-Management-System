c7b706f9303048a840656a8925296becae1cf7cc
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-25 18:35:44 +0545

    Added:dashboard view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class DashboardRedirectView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if hasattr(user, 'profile'):
+            if user.profile.role == 'LANDLORD':
+                return redirect('dashboard:landlord_dash')
+            elif user.profile.role == 'TENANT':
+                return redirect('dashboard:tenant_dash')
+        # Default or error case
+        return redirect('admin:index')
