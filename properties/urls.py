f7e29911f300fb569a585b20ba6a65dc9f074625
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-26 20:08:54 +0545

    Route for property and room

--- properties/urls.py
+++ properties/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PropertyListView, RoomListView
+from .views import PropertyListView, RoomListView, PropertyCreateView, RoomCreateView
 
 app_name = 'properties'
 
 urlpatterns = [
     path('', PropertyListView.as_view(), name='property_list'),
+    path('add/', PropertyCreateView.as_view(), name='property_add'),
     path('rooms/', RoomListView.as_view(), name='room_list'),
+    path('rooms/add/', RoomCreateView.as_view(), name='room_add'),
 ]
