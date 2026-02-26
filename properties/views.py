ef0487685f4c444e4ab0fd76fb5273cb139e3f17
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-26 20:08:21 +0545

    Updated view with create property and room

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from core.mixins import LandlordRequiredMixin
 from .models import Property, Room
+from .forms import PropertyForm, RoomForm
 
 class PropertyListView(LandlordRequiredMixin, ListView):
     model = Property
@@ -12,6 +15,17 @@ class PropertyListView(LandlordRequiredMixin, ListView):
     def get_queryset(self):
         return Property.objects.filter(landlord=self.request.user)
 
+class PropertyCreateView(LandlordRequiredMixin, CreateView):
+    model = Property
+    form_class = PropertyForm
+    template_name = 'properties/property_form.html'
+    success_url = reverse_lazy('properties:property_list')
+
+    def form_valid(self, form):
+        form.instance.landlord = self.request.user
+        messages.success(self.request, "Property created successfully!")
+        return super().form_valid(form)
+
 class RoomListView(LandlordRequiredMixin, ListView):
     model = Room
     template_name = 'properties/room_list.html'
@@ -19,3 +33,18 @@ class RoomListView(LandlordRequiredMixin, ListView):
 
     def get_queryset(self):
         return Room.objects.filter(property__landlord=self.request.user)
+
+class RoomCreateView(LandlordRequiredMixin, CreateView):
+    model = Room
+    form_class = RoomForm
+    template_name = 'properties/room_form.html'
+    success_url = reverse_lazy('properties:room_list')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['landlord'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        messages.success(self.request, "Room created successfully!")
+        return super().form_valid(form)
