46320b579dda53116a896a2840a19a19c1ab7734
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-27 19:32:50 +0545

    Added Edit and delete proprty view

--- properties/views.py
+++ properties/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -26,6 +26,31 @@ class PropertyCreateView(LandlordRequiredMixin, CreateView):
         messages.success(self.request, "Property created successfully!")
         return super().form_valid(form)
 
+class PropertyUpdateView(LandlordRequiredMixin, UpdateView):
+    model = Property
+    form_class = PropertyForm
+    template_name = 'properties/property_form.html'
+    success_url = reverse_lazy('properties:property_list')
+
+    def get_queryset(self):
+        return Property.objects.filter(landlord=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, "Property updated successfully!")
+        return super().form_valid(form)
+
+class PropertyDeleteView(LandlordRequiredMixin, DeleteView):
+    model = Property
+    template_name = 'properties/property_confirm_delete.html'
+    success_url = reverse_lazy('properties:property_list')
+
+    def get_queryset(self):
+        return Property.objects.filter(landlord=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, "Property deleted successfully!")
+        return super().form_valid(form)
+
 class RoomListView(LandlordRequiredMixin, ListView):
     model = Room
     template_name = 'properties/room_list.html'
@@ -47,4 +72,34 @@ class RoomCreateView(LandlordRequiredMixin, CreateView):
 
     def form_valid(self, form):
         messages.success(self.request, "Room created successfully!")
+        return super().form_valid(form)
+
+class RoomUpdateView(LandlordRequiredMixin, UpdateView):
+    model = Room
+    form_class = RoomForm
+    template_name = 'properties/room_form.html'
+    success_url = reverse_lazy('properties:room_list')
+
+    def get_queryset(self):
+        return Room.objects.filter(property__landlord=self.request.user)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['landlord'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        messages.success(self.request, "Room updated successfully!")
+        return super().form_valid(form)
+
+class RoomDeleteView(LandlordRequiredMixin, DeleteView):
+    model = Room
+    template_name = 'properties/room_confirm_delete.html'
+    success_url = reverse_lazy('properties:room_list')
+
+    def get_queryset(self):
+        return Room.objects.filter(property__landlord=self.request.user)
+
+    def form_valid(self, form):
+        messages.success(self.request, "Room deleted successfully!")
         return super().form_valid(form)
