82aebcc07c9b0377726a9a93d09aa555a4758204
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-27 19:46:26 +0545

    Added route mark paid

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import InvoiceListView, GenerateInvoicesView
+from .views import InvoiceListView, GenerateInvoicesView, MarkInvoicePaidView, InvoiceDetailView
 
 app_name = 'payments'
 
 urlpatterns = [
     path('', InvoiceListView.as_view(), name='invoice_list'),
     path('generate-invoices/', GenerateInvoicesView.as_view(), name='generate_invoices'),
+    path('<int:pk>/', InvoiceDetailView.as_view(), name='invoice_detail'),
+    path('<int:pk>/mark-paid/', MarkInvoicePaidView.as_view(), name='mark_paid'),
 ]
