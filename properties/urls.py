4dab357aef65613b896c4df0e303b011d2a62b80
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 16:19:43 +0545

    Added tenancy route

--- properties/urls.py
+++ properties/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     PropertyListView, PropertyCreateView, PropertyUpdateView, PropertyDeleteView,
     RoomListView, RoomCreateView, RoomUpdateView, RoomDeleteView,
+    TenancyListView, TenancyCreateView, TenancyUpdateView, TenancyDeleteView,
 )
 
 app_name = 'properties'
@@ -15,4 +16,8 @@ urlpatterns = [
     path('rooms/add/', RoomCreateView.as_view(), name='room_add'),
     path('rooms/<int:pk>/edit/', RoomUpdateView.as_view(), name='room_edit'),
     path('rooms/<int:pk>/delete/', RoomDeleteView.as_view(), name='room_delete'),
+    path('tenancies/', TenancyListView.as_view(), name='tenancy_list'),
+    path('tenancies/add/', TenancyCreateView.as_view(), name='tenancy_add'),
+    path('tenancies/<int:pk>/edit/', TenancyUpdateView.as_view(), name='tenancy_edit'),
+    path('tenancies/<int:pk>/delete/', TenancyDeleteView.as_view(), name='tenancy_delete'),
 ]
