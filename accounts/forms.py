9e7d59ae4ac892a41bd58c49281f2bb881472f95
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 17:08:52 +0545

    Added password change

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import PasswordChangeForm
+
+class TenantPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
