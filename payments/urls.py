06b548c85a2a8e5ad1b28d954b3e922c331a8a8c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 16:17:30 +0545

    Added route for process payment

--- payments/urls.py
+++ payments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import InvoiceListView, GenerateInvoicesView, MarkInvoicePaidView, InvoiceDetailView
+from .views import InvoiceListView, GenerateInvoicesView, MarkInvoicePaidView, InvoiceDetailView, ProcessPaymentView
 
 app_name = 'payments'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('generate-invoices/', GenerateInvoicesView.as_view(), name='generate_invoices'),
     path('<int:pk>/', InvoiceDetailView.as_view(), name='invoice_detail'),
     path('<int:pk>/mark-paid/', MarkInvoicePaidView.as_view(), name='mark_paid'),
+    path('<int:pk>/process-payment/', ProcessPaymentView.as_view(), name='process_payment'),
 ]
