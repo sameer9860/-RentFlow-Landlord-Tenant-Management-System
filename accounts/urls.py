88994a1fa66b0ca90713fe184f393eadf278ca7d
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-01 16:24:01 +0545

    Added dashboard url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from .views import DashboardRedirectView, TenantPasswordChangeView
 app_name = 'accounts'
 
 urlpatterns = [
+    path('', DashboardRedirectView.as_view(), name='dashboard_redirect_root'),
     path('dashboard-redirect/', DashboardRedirectView.as_view(), name='dashboard_redirect'),
     path('password-change/', TenantPasswordChangeView.as_view(), name='password_change'),
 ]
