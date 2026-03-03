e4124e5187753cef5eb4c7978087c48d69bd33ff
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-03 20:02:02 +0545

    Added profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DashboardRedirectView, TenantPasswordChangeView
+from .views import DashboardRedirectView, TenantPasswordChangeView, profile_view
 
 app_name = 'accounts'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', DashboardRedirectView.as_view(), name='dashboard_redirect_root'),
     path('dashboard-redirect/', DashboardRedirectView.as_view(), name='dashboard_redirect'),
     path('password-change/', TenantPasswordChangeView.as_view(), name='password_change'),
+    path('profile/<int:profile_id>/', profile_view, name='profile_view'),
 ]
